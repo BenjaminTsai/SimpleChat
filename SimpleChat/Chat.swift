e2ee8cb783df09b7424379c93e8efa2ed73f8112
Benjamin Tsai <hello@benjamintsai.com>
2015-06-03 18:53:41 -0700

    Initial fields for Chat and Message model

--- SimpleChat/Chat.swift
+++ SimpleChat/Chat.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class Chat: NSObject {
     
-
+    var sender: Account?
+    var receiver: Account?
+    var messages: [Message]?
    
 }
--- SimpleChat/Message.swift
+++ SimpleChat/Message.swift
@@ -10,4 +10,7 @@ import UIKit
 
 class Message: NSObject {
    
+    var body: String?
+    var timestamp: NSDate?
+    
 }
